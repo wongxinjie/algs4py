ff870f093539a37c3ba83348397497b768b8728e
wongxinjie <wongxinjierun@gmail.com>
2017-11-05 22:41:23 +0800

    add avl trees

--- algs/searching/search_trees.py
+++ algs/searching/search_trees.py
@@ -14,10 +14,10 @@ class TreeNode:
         return self.right_child
 
     def is_left_child(self):
-        return self.parent and self.parent.left == self
+        return self.parent and self.parent.left_child == self
 
     def is_right_child(self):
-        return self.parent and self.parent.right == self
+        return self.parent and self.parent.right_child == self
 
     def is_root(self):
         return not self.parent
@@ -209,8 +209,109 @@ class BinraySearchTree:
                                       node.right_child.right_child)
 
 
+class AVLTreeNode(TreeNode):
+
+    def __init__(self, key, value, left=None, right=None, parent=None, balance_factor=0):
+        self.balance_factor = balance_factor
+        super().__init__(key, value, left, right, parent)
+
+
+class AVLTree(BinraySearchTree):
+
+    def put(self, key, value):
+        if self.root:
+            self._put(key, value, self.root)
+        else:
+            self.root = AVLTreeNode(key, value)
+        self.size += 1
+
+    def _put(self, key, value, node):
+        if key < node.key:
+            if node.has_left_child():
+                self._put(key, value, node.left_child)
+            else:
+                node.left_child = AVLTreeNode(key, value, parent=node)
+                self.update_balance(node.left_child)
+        else:
+            if node.has_right_child():
+                self._put(key, value, node.right_child)
+            else:
+                node.right_child = AVLTreeNode(key, value, parent=node)
+                self.update_balance(node.right_child)
+
+    def update_balance(self, node):
+        if node.balance_factor > 1 or node.balance_factor < -1:
+            self.rebalance(node)
+            return
+
+        if node.parent is not None:
+            if node.is_left_child():
+                node.parent.balance_factor += 1
+            elif node.is_right_child():
+                node.parent.balance_factor -= 1
+
+            if node.parent.balance_factor != 0:
+                self.update_balance(node.parent)
+
+    def rotate_left(self, root):
+        new_root = root.right_child
+        root.right_child = new_root.left_child
+        if new_root.left_child is not None:
+            new_root.left_child.parent = root
+        new_root.parent = root.parent
+
+        if root.is_root():
+            self.root = new_root
+        else:
+            if root.is_left_child():
+                root.parent.left_child = new_root
+            else:
+                root.parent.right_child = new_root
+
+        new_root.left_child = root
+        root.parent = new_root
+
+        root.balance_factor = root.balance_factor + 1 - min(new_root.balance_factor, 0)
+        new_root.balance_factor = new_root.balance_factor + 1 + max(root.balance_factor, 0)
+
+    def rotate_right(self, root):
+        new_root = root.left_child
+        root.left_child = new_root.right_child
+        if new_root.right_child is not None:
+            new_root.right_child.parent = root
+        new_root.parent = root.parent
+
+        if root.is_root():
+            self.root = new_root
+        else:
+            if root.is_left_child():
+                root.parent.left_child = new_root
+            else:
+                root.parent.right_child = new_root
+
+        new_root.right_child = root
+        root.parent = new_root
+        root.balance_factor = root.balance_factor - 1 + min(new_root.balance_factor, 0)
+        new_root.balance_factor = new_root.balance_factor - 1 - max(root.balance_factor, 0)
+
+    def rebalance(self, node):
+        if node.balance_factor < 0:
+            if node.right_child.balance_factor > 0:
+                self.rotate_right(node.right_child)
+                self.rotate_left(node)
+            else:
+                self.rotate_left(node)
+        elif node.balance_factor > 0:
+            if node.left_child.balance_factor < 0:
+                self.rotate_left(node.left_child)
+                self.rotate_right(node)
+            else:
+                self.rotate_right(node)
+
+
 if __name__ == "__main__":
-    tree = BinraySearchTree()
+    # tree = BinraySearchTree()
+    tree = AVLTree()
     tree[3] = 'Python'
     tree[0] = 'C'
     tree[1] = 'C++'
@@ -219,6 +320,6 @@ if __name__ == "__main__":
     tree[5] = 'Javascript'
 
     print(tree[2])
-    del tree[5]
+    # del tree[5]
     for n in tree:
         print(n)
